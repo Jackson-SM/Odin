ed5739a82c467c05aff0f4a582e75041b305f060
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2024-01-02 23:11:29 -0300

    feat: add theme colors

---
+++ src/config/stitches.config.ts
@@ -0,0 +1,13 @@
+import { createStitches } from '@stitches/react'
+
+export const { styled } = createStitches({
+  theme: {
+    colors: {
+      background: '#000500',
+      backgroundLight: '#292929',
+
+      primary: '#f96e46',
+      secondary: '#473bf0',
+    },
+  },
+})
